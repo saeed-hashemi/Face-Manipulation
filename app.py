51e9fea798c9c5c4507fcfc629967b87e0b861b8
saeed hashemi <saeedhashemi.es@gmail.com>
2021-11-10 09:11:32 +0330

    Checking the input image if exist or not.

--- app.py
+++ app.py
@@ -21,28 +21,31 @@ if __name__ == '__main__':
     # predictor = dlib.shape_predictor('model/shape_predictor_68_face_landmarks.dat')
 
     image = cv2.imread(args["image"])
-    # image = cv2.GaussianBlur(image, (11, 11), 0)
-    image = cv2.resize(image, (round(
-        image.shape[1]*float(args["resize"])), round(image.shape[0]*float(args["resize"]))), interpolation=cv2.INTER_AREA)
+    if image is not None:
+        # image = cv2.GaussianBlur(image, (11, 11), 0)
+        image = cv2.resize(image, (round(
+            image.shape[1]*float(args["resize"])), round(image.shape[0]*float(args["resize"]))), interpolation=cv2.INTER_AREA)
 
-    # initilize the resize ratio for nose and left eye
-    resize_ratio = int(args["threshold"])/200
+        # initilize the resize ratio for nose and left eye
+        resize_ratio = int(args["threshold"])/200
 
-    elements = ["nose", "left_eye"]
+        elements = ["nose", "left_eye"]
 
-    # Convert the image color to grayscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Convert the image color to grayscale
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # Detect the face
-    rects = detector(gray, 1)
+        # Detect the face
+        rects = detector(gray, 1)
 
-    # Detect landmarks for each face (if there is more that one face in a frame)
-    for rect in rects:
-        shape = find_face_landmarks(gray, rect)
+        # Detect landmarks for each face (if there is more that one face in a frame)
+        for rect in rects:
+            shape = find_face_landmarks(gray, rect)
 
-        if shape.any():
-            for element in elements:
-                image = resize_element(image, shape, resize_ratio, element)
-            cv2.imwrite(f"result.jpg", image)
-        else:
-            print("Nothing found!")
+            if shape.any():
+                for element in elements:
+                    image = resize_element(image, shape, resize_ratio, element)
+                cv2.imwrite(f"result.jpg", image)
+            else:
+                print("Nothing found!")
+    else:
+        print("The image does not exist!")
